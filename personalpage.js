121f6e8cdfc1897bf764a301118ddeafa7dd4258
shihyunchen <30455607+shihyunchen@users.noreply.github.com>
2017-08-02 23:59:25 +0800

    Add files via upload

--- personalpage.js
+++ personalpage.js
@@ -1,43 +1,49 @@
 $(document).ready(function() {
-    $("#homebar").click(function(event) {
+    $("#homebar").click(function(event)) {
         event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#home").offset().top
         }, 1300, 'easeInOutExpo');
     });  
-    $("#welcomebtn").click(function(event) {
+    $("#welcomebtn").click(function(event)) {
         event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#author").offset().top
         }, 1300, 'easeInOutExpo');
     });
-    $("#authorbar").click(function() {
+    $("#authorbar").click(function(event)) {
+        event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#author").offset().top
         }, 1300, 'easeInOutExpo');
     });
      
-    $("#introbar").click(function() {
+    $("#introbar").click(function(event)) {
+        event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#introduction").offset().top
         }, 1300, 'easeInOutExpo');
     });
-    $("#expbar").click(function() {
+    $("#expbar").click(function(event)) {
+        event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#experience").offset().top
         }, 1300, 'easeInOutExpo');
     });
-    $("#storybar").click(function() {
+    $("#storybar").click(function(event)) {
+        event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#story").offset().top
         }, 1300, 'swing');
     });
-    $("#friendsbar").click(function() {
+    $("#friendsbar").click(function(event)) {
+        event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#friends").offset().top
         }, 1300, 'easeInOutExpo');
     });
-    $("#famousbar").click(function() {
+    $("#famousbar").click(function(event)) {
+        event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#famous").offset().top
         }, 1300, 'easeInOutExpo');
