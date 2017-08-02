d99380b056802c88381ea16ef8ceb944189798bb
shihyunchen <30455607+shihyunchen@users.noreply.github.com>
2017-08-02 14:53:45 +0800

    Add files via upload

--- personalpage.js
+++ personalpage.js
@@ -3,37 +3,37 @@ $(document).ready(function() {
         event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#home").offset().top
-        }, 1300, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
     });
     $("#authorbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#author").offset().top
-        }, 1300, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
     });
     $("#introbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#introduction").offset().top
-        }, 1300, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
     });
     $("#expbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#experience").offset().top
-        }, 1300, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
     });
     $("#storybar").click(function() {
         $('html, body').animate({
             scrollTop: $("#story").offset().top
-        }, 1300, 'swing');
+        }, 500, 'swing');
     });
     $("#friendsbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#friends").offset().top
-        }, 1300, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
     });
     $("#famousbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#famous").offset().top
-        }, 1300, 'easeInOutExpo');
+        }, 500, 'easeInOutExpo');
     });
 
 });
