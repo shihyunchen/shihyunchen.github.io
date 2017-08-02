62229730e0f59cba60d438e5d8b6a7829d5ef759
shihyunchen <30455607+shihyunchen@users.noreply.github.com>
2017-08-02 14:57:24 +0800

    Add files via upload

--- personalpage.js
+++ personalpage.js
@@ -3,22 +3,22 @@ $(document).ready(function() {
         event.preventDefault();
         $('html, body').animate({
             scrollTop: $("#home").offset().top
-        }, 500, 'easeInOutExpo');
+        }, 500, 'swing');
     });
     $("#authorbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#author").offset().top
-        }, 500, 'easeInOutExpo');
+        }, 500, 'swing');
     });
     $("#introbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#introduction").offset().top
-        }, 500, 'easeInOutExpo');
+        }, 500, 'swing');
     });
     $("#expbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#experience").offset().top
-        }, 500, 'easeInOutExpo');
+        }, 500, 'swing');
     });
     $("#storybar").click(function() {
         $('html, body').animate({
@@ -28,12 +28,12 @@ $(document).ready(function() {
     $("#friendsbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#friends").offset().top
-        }, 500, 'easeInOutExpo');
+        }, 500, 'swing');
     });
     $("#famousbar").click(function() {
         $('html, body').animate({
             scrollTop: $("#famous").offset().top
-        }, 500, 'easeInOutExpo');
+        }, 500, 'swing');
     });
 
 });
